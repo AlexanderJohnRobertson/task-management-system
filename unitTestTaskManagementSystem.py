8bd343518a6cf41c560ea7a5f0f05a1b534b49d7
AlexanderJohnRobertson <s275931@uos.ac.uk>
2024-02-27 14:13:22 +0000

    added all unit tests

--- unitTestTaskManagementSystem.py
+++ unitTestTaskManagementSystem.py
@@ -20,6 +20,78 @@ class TestRoutes(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Access Denied', response.data)
 
+    def test_view_task(self):
+        response = self.app.get('viewtasks')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_update_task(self):
+        response = self.app.get('updatetask')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_delete_task(self):
+        response = self.app.get('deletetask')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_create_project(self):
+        response = self.app.get('addproject')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_view_projects(self):
+        response = self.app.get('viewprojects')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_update_project(self):
+        response = self.app.get('updateproject')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_delete_project(self):
+        response = self.app.get('deleteproject')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_account_details(self):
+        response = self.app.get('accountdetails')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_update_account_details(self):
+        response = self.app.get('updateaccountdetails')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_change_username(self):
+        response = self.app.get('changeusername')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_change_password(self):
+        response = self.app.get('changepassword')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_view_account_details(self):
+        response = self.app.get('accountdetails')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Access Denied', response.data)
+
+    def test_user_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'', response.data)
+
+    def test_user_home(self):
+        response = self.app.get('userhome/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'', response.data)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
